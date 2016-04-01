64e3f5b1cd30e10f55f3bff4ef6af78602fa6931
Mito Santos <Mito Santos>
2016-04-01 14:48:54 -0600

    Examen 2 Lab

---
+++ android/src/com/koalio/game/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.koalio.game;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.koalio.game.MyKoalioGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new MyKoalioGame(), config);
+	}
+}
